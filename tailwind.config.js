d453a2421253665e6bf048321ccb98a7e2671c59
sobhan <sobhan168nourani@gmail.com>
2024-11-15 17:38:48 +0330

    complete course category section of main page and edit navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,7 @@ module.exports = {
       'b-fatemi': ['b-fatemi'],
       'dagon': ['dagon'],
       'khodkar': ['khodkar'],
+      'radin2': ['radin2'],
       'radin3': ['radin3'],
       'battle-bingo': ['battle-bingo'],
     }
