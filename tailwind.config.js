2df100a1f0f67de752f338b6f45c7344bdff2976
sobhan <sobhan168nourani@gmail.com>
2024-11-15 14:49:02 +0330

    complete a header section of main page

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./src/*.html"],
   theme: {
-    extend: {}
+    extend: {},
+    fontFamily: {
+      'dana-demibold': ['dana-demibold'],
+      'b-fatemi': ['b-fatemi'],
+      'dagon': ['dagon'],
+      'khodkar': ['khodkar'],
+      'radin3': ['radin3'],
+      'battle-bingo': ['battle-bingo'],
+    }
   },
   plugins: [],
 }
